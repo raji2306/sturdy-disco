b820cb98af637c4b33c551fb476f1fce4feb56bc
raji2306 <94977452+raji2306@users.noreply.github.com>
2023-03-12 12:20:16 +0530

    Create fileWrite.groovy

---
+++ vars/fileWrite.groovy
@@ -0,0 +1,10 @@
+def fileWrite(string filename){
+  def file = new File(filename)
+  def writer = new FileWriter(file, true)
+    if(writer.canRead()){ 
+      writer.write("Hello, Everything will be alright. Be calm")
+      writer.close()
+    }  
+    else {
+      echo "Sorry, The requested file is not writable"
+}
