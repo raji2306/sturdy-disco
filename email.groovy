db08ecd449c3cc96244f392c65680e4e469f37ba
raji2306 <94977452+raji2306@users.noreply.github.com>
2023-09-26 16:30:54 +0530

    Created email.groovy

---
+++ email.groovy
@@ -0,0 +1,9 @@
+def recipients = "rajeshsuresh154@gmail.com, rajeshsuresh230699@gmail.com"
+def subject = "Build ${BUILD_NUMBER} in ${JOB_NAME} - ${BUILD_STATUS}"
+def body = """
+<p>Build Result: ${BUILD_RESULT}</p>
+<p>Project: ${PROJECT_NAME}</p>
+<p>Build URL: ${BUILD_URL}</p>
+"""
+
+emailext(to: recipients, subject: subject, body: body)
