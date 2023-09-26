d8ebfad82ef6768c2fadf53ebe0596e45fb4050e
raji2306 <94977452+raji2306@users.noreply.github.com>
2023-09-26 17:49:52 +0530

    Update email.groovy

--- email.groovy
+++ email.groovy
@@ -1,12 +1,16 @@
-// emailConfig.groovy
 def call(Map config) {
     def recipients = "rajeshsuresh154@gmail.com, rajeshsuresh230699@gmail.com"
     def subject = "Build ${config.buildNumber} in ${config.jobName} - ${config.buildResult}"
     def body = """
-    <p>Build Result: ${config.buildResult}</p>
-    <p>Project: ${config.jobName}</p>
-    <p>Build URL: ${config.buildUrl}</p>
+    Build Result: ${config.buildResult}
+    Project: ${config.jobName}
+    Build URL: ${config.buildUrl}
     """
 
-    emailext(to: recipients, subject: subject, body: body)
+    // Use the mail step to send the email
+    mail(
+        to: recipients,
+        subject: subject,
+        body: body
+    )
 }
