e860703520e9f7f58d95e56525fa5b9a8d02d6da
raji2306 <94977452+raji2306@users.noreply.github.com>
2023-09-26 17:51:51 +0530

    Update email.groovy

--- email.groovy
+++ email.groovy
@@ -1,9 +1,15 @@
-def recipients = "rajeshsuresh154@gmail.com, rajeshsuresh230699@gmail.com"
-def buildNumber = env.BUILD_NUMBER
-def jobName = env.JOB_NAME
-def buildResult = currentBuild.resultIsBetterOrEqualTo('SUCCESS') ? 'SUCCESS' : 'FAILURE'
-def buildUrl = env.BUILD_URL
+post {
+    always {
+        // Define and pass the necessary environment variables
+        script {
+            def recipients = "rajeshsuresh154@gmail.com, rajeshsuresh230699@gmail.com"
+            def buildNumber = env.BUILD_NUMBER
+            def jobName = env.JOB_NAME
+            def buildResult = currentBuild.resultIsBetterOrEqualTo('SUCCESS') ? 'SUCCESS' : 'FAILURE'
+            def buildUrl = env.BUILD_URL
 
-emailScript(buildNumber: buildNumber, jobName: jobName, buildResult: buildResult, buildUrl: buildUrl)
-
-// Define a separate stage or step for the email sending logic if needed.
+            // Call the emailScript step with named arguments
+            emailScript(buildNumber: buildNumber, jobName: jobName, buildResult: buildResult, buildUrl: buildUrl)
+        }
+    }
+}
