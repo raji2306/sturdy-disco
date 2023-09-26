6e770df17921ac8986e00d2d80cd0ae5516c2a8e
raji2306 <94977452+raji2306@users.noreply.github.com>
2023-09-26 17:07:24 +0530

    Update email.groovy

--- email.groovy
+++ email.groovy
@@ -1,9 +1,10 @@
+// emailConfig.groovy
 def recipients = "rajeshsuresh154@gmail.com, rajeshsuresh230699@gmail.com"
-def subject = "Build ${BUILD_NUMBER} in ${JOB_NAME}"
+def subject = "Build ${buildNumber} in ${jobName} - ${buildResult}"
 def body = """
-<p>Build Result: ${BUILD_RESULT}</p>
-<p>Project: ${PROJECT_NAME}</p>
-<p>Build URL: ${BUILD_URL}</p>
+<p>Build Result: ${buildResult}</p>
+<p>Project: ${jobName}</p>
+<p>Build URL: ${buildUrl}</p>
 """
 
 emailext(to: recipients, subject: subject, body: body)
